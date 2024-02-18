0b24ba2c23c4221dbc2f90d622a31776fa4e9c17
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-02-18 22:45:40 +0000

    Cleaning up serve_file function

---
+++ src/server.rs
@@ -0,0 +1,214 @@
+use crate::{
+    config::{Config, Rewrite},
+    error::ChimneyError::{
+        self, FailedToAcceptConnection, FailedToBind, FailedToParseAddress, UnableToOpenFile,
+    },
+    log_error, log_info, log_request,
+};
+use bytes::Bytes;
+use futures_util::stream::TryStreamExt;
+use http_body_util::{combinators::BoxBody, BodyExt, Full, StreamBody};
+use hyper::{
+    body::Frame,
+    header::{HeaderName, HeaderValue},
+    server::conn::http1,
+    service::service_fn,
+    Request, Response, Result as HyperResult, StatusCode,
+};
+use hyper_util::rt::TokioIo;
+use std::{net::SocketAddr, path::PathBuf, str::FromStr, sync::Arc};
+use tokio::{fs::File, net::TcpListener, sync::Notify};
+use tokio_util::io::ReaderStream;
+
+#[derive(Debug, Clone)]
+pub struct Server {
+    pub config: Config,
+    shutdown_signal: Arc<Notify>,
+}
+
+impl Server {
+    pub fn new(config: Config) -> Self {
+        Server {
+            config,
+            shutdown_signal: Arc::new(Notify::new()),
+        }
+    }
+
+    pub async fn run(self) -> Result<(), ChimneyError> {
+        self.watch_for_shutdown_signal().await;
+        self.listen().await?;
+
+        Ok(())
+    }
+
+    async fn watch_for_shutdown_signal(&self) {
+        let signal = self.shutdown_signal.clone();
+
+        tokio::spawn(async move {
+            tokio::signal::ctrl_c()
+                .await
+                .expect("Failed to listen for shutdown signal");
+
+            signal.notify_one();
+        });
+    }
+
+    // TODO: handle HTTPS (run a second server for HTTPS, and force redirect from HTTP to
+    // HTTPS if enabled)
+    async fn listen(self) -> Result<(), ChimneyError> {
+        let raw_addr = format!("{}:{}", self.config.host, self.config.port);
+        let addr: SocketAddr = raw_addr
+            .parse()
+            .map_err(|e| FailedToParseAddress(raw_addr, e))?;
+
+        let server = TcpListener::bind(addr).await.map_err(FailedToBind)?;
+
+        log_info!(format!(
+            "Server is running at http://{}:{}",
+            self.config.host, self.config.port
+        ));
+
+        loop {
+            let self_clone = self.clone();
+
+            tokio::select! {
+                _ = self.shutdown_signal.notified() => {
+                    println!("\n");
+                    log_info!("Shutting down server");
+                    return Ok(());
+                },
+
+                res = server.accept() => {
+                    if let Err(error) = res {
+                        log_error!(error);
+                        return Err(FailedToAcceptConnection(error));
+                    }
+
+                    tokio::spawn(async move {
+                        let (stream, _) = res.unwrap();
+                        let io = TokioIo::new(stream);
+                        let service = service_fn(|req| serve_file(&self_clone, req));
+                        let conn = http1::Builder::new().serve_connection(io, service);
+
+                        if let Err(error) = conn.await {
+                            log_error!(error);
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    pub fn find_rewrite_or(&self, target: &str) -> String {
+        if self.config.rewrites.is_empty() {
+            return target.to_string();
+        }
+
+        let rewrite_key = if target.starts_with('/') {
+            target.to_string()
+        } else {
+            format!("/{}", target)
+        };
+
+        assert!(rewrite_key.starts_with('/'));
+
+        if let Some(rewrite) = self.config.rewrites.get(&rewrite_key) {
+            return match rewrite {
+                Rewrite::Config { to } => to,
+                Rewrite::Target(target) => target,
+            }
+            .to_string();
+        };
+
+        target.to_string()
+    }
+
+    pub fn get_file_path(&self, target: &str) -> Option<PathBuf> {
+        let path = PathBuf::from(&self.config.root_dir).join(target.trim_start_matches('/'));
+
+        if !path.exists() {
+            if let Some(fallback) = self.config.fallback_document.clone() {
+                let fallback_path = PathBuf::from(&self.config.root_dir).join(fallback);
+                if fallback_path.exists() {
+                    return Some(fallback_path);
+                }
+            } else {
+                return None;
+            }
+        };
+
+        Some(path)
+    }
+
+    pub fn build_response(
+        &self,
+        boxed_body: BoxBody<Bytes, std::io::Error>,
+    ) -> Response<BoxBody<Bytes, std::io::Error>> {
+        todo!()
+    }
+}
+
+fn not_found() -> Response<BoxBody<Bytes, std::io::Error>> {
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Full::new("".into()).map_err(|e| match e {}).boxed())
+        .unwrap()
+}
+
+// TODO: cleanup
+async fn serve_file(
+    server: &Server,
+    req: Request<hyper::body::Incoming>,
+) -> HyperResult<Response<BoxBody<Bytes, std::io::Error>>> {
+    if server.config.enable_logging {
+        log_request!(&req);
+    }
+
+    // We need to automatically rewrite `/` to `/index.html` if the path is empty since they are
+    // generally considered one and the same
+    let request_path = if req.uri().path().trim_end_matches("/").is_empty() {
+        "/index.html"
+    } else {
+        req.uri().path()
+    };
+
+    let target = server.find_rewrite_or(request_path);
+    let path = match server.get_file_path(target.as_str()) {
+        Some(path) => path,
+        None => {
+            return Ok(not_found());
+        }
+    };
+
+    // impl: https://github.com/hyperium/hyper/blob/00a703a9ef268266f8a8f78540253cbb2dcc6a55/examples/send_file.rs#L67-L91
+    let file: File = match File::open(path).await.map_err(UnableToOpenFile) {
+        Ok(file) => file,
+        Err(error) => {
+            log_error!(format!("Failed to open file: {:?}", error));
+            return Ok(not_found());
+        }
+    };
+
+    let reader_stream = ReaderStream::new(file);
+    let boxed_body = StreamBody::new(reader_stream.map_ok(Frame::data)).boxed();
+
+    // TODO: refactor into own function (file_to_response)
+    //
+    // TODO: fetch proper mimetype
+    let mut response = Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "text/html")
+        .header("Server", "Chimney")
+        .header("X-Content-Type-Options", "nosniff");
+
+    if let Some(headers) = response.headers_mut() {
+        for (key, value) in server.config.headers.iter() {
+            if let Ok(header_name) = HeaderName::from_str(key) {
+                headers.insert(header_name, HeaderValue::from_str(value).unwrap());
+            }
+        }
+    }
+
+    // TODO: don't just unwrap here
+    Ok(response.body(boxed_body).unwrap())
+}
