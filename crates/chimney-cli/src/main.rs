29a30c7755f6e19ac6c98aa1b002691a599367da
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-06-06 00:48:16 +0100

    Setup main function with tokio entrypoint

--- crates/chimney-cli/src/main.rs
+++ crates/chimney-cli/src/main.rs
@@ -3,5 +3,10 @@ pub(crate) mod cli;
 #[tokio::main]
 async fn main() {
     let cli = cli::Cli::new();
-    todo!()
+
+    if let Err(e) = cli.run().await {
+        dbg!(&e);
+        log::error!("Error: {}", e);
+        std::process::exit(1);
+    }
 }
