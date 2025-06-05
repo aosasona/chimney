246176a5acd0959b7e3b9eb3263c261fece60f15
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-06-05 23:28:41 +0100

    Use log package instead

--- crates/chimney-core/src/server.rs
+++ crates/chimney-core/src/server.rs
@@ -1,7 +1,6 @@
-pub struct Server {
-    /// Whether to run the server in debug mode
-    debug: bool,
+use log::debug;
 
+pub struct Server {
     /// The filesystem abstraction used by the server
     filesystem: Box<dyn crate::filesystem::Filesystem>,
 
@@ -9,52 +8,22 @@ pub struct Server {
     config: crate::config::Config,
 }
 
-macro_rules! init_tracing {
-    ($debug:expr) => {
-        let log_level = if $debug {
-            tracing::Level::DEBUG
-        } else {
-            tracing::Level::INFO
-        };
-
-        let _ = tracing_subscriber::fmt()
-            .with_thread_ids(true)
-            .with_thread_names(true)
-            .with_level(true)
-            .with_target(false)
-            .with_file(true)
-            .with_line_number(true)
-            .with_max_level(log_level)
-            .try_init();
-    };
-}
-
 impl Server {
     pub fn new(
-        debug: bool,
         filesystem: Box<dyn crate::filesystem::Filesystem>,
         config: crate::config::Config,
     ) -> Self {
-        // TODO: create an instance-level tracing subscriber instead of a global one here
-        // TODO: remove `tracng_subscriber` dependency as recommended by the docs
-
-        // Initialize tracing for the server
-        init_tracing!(debug);
-
-        Server {
-            debug,
-            filesystem,
-            config,
-        }
+        debug!("Creating a new Chimney server instance");
+        Server { filesystem, config }
     }
 
     pub async fn run(&self) -> Result<(), crate::error::ChimneyError> {
         // Here you would implement the logic to start the server
         // For now, we just print the configuration and return Ok
-        if self.debug {
-            dbg!(&self.config);
-            println!("Running in debug mode");
-        }
+        debug!(
+            "Running in debug mode with configuration: {:?}",
+            self.config
+        );
 
         unimplemented!("Implement server logic here");
     }
