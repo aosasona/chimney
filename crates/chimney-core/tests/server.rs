56986bfe4b36dbbf5cc935450f3496d2404e21a8
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-06-12 23:09:56 +0100

    Add more socket_address tests

--- crates/chimney-core/tests/server.rs
+++ crates/chimney-core/tests/server.rs
@@ -1,34 +1,91 @@
-use chimney::{Server, config::Config, filesystem};
+use chimney::{
+    Server,
+    config::{Config, Format, toml},
+    filesystem,
+};
+
+macro_rules! test_socket_address {
+    ($config:expr) => {
+        let server = mock_server($config);
+        let addr = server.get_socket_address();
+        assert!(
+            addr.is_ok(),
+            "Failed to get socket address: {:?}",
+            addr.err()
+        );
+
+        let socket_addr = addr.unwrap();
+        let exptected_addr = format!("{}:{}", $config.host, $config.port);
+        assert_eq!(
+            socket_addr.to_string(),
+            exptected_addr,
+            "Socket address does not match expected: {} != {}",
+            socket_addr,
+            exptected_addr
+        );
+    };
+    ($config:expr, reserved) => {
+        let server = mock_server($config);
+        let addr = server.get_socket_address();
+        assert!(
+            addr.is_err(),
+            "Expected an error for config, but got: {:?}",
+            addr
+        );
+    };
+}
+
+macro_rules! config {
+    ($input:expr) => {{
+        let toml_config = toml::Toml::from($input);
+        toml_config.parse().expect("Failed to parse TOML config")
+    }};
+    () => {
+        mock_config()
+    };
+}
 
 fn mock_config() -> Config {
     Config::default()
 }
 
-fn mock_server() -> Server {
+fn mock_server(config: Config) -> Server {
     Server::new(
         chimney::config::LogLevel::Trace,
         filesystem::mock::new(),
-        mock_config(),
+        config,
     )
 }
 
 #[test]
-pub fn test_get_socket_address() {
-    let server = mock_server();
-    let addr = server.get_socket_address();
-    assert!(
-        addr.is_ok(),
-        "Failed to get socket address: {:?}",
-        addr.err()
-    );
+// Test with the mock server configuration
+pub fn test_get_socket_address_with_default_config() {
+    test_socket_address!(mock_config());
+}
 
-    let socket_addr = addr.unwrap();
-    let exptected_addr = format!("{}:{}", server.config().host, server.config().port);
-    assert_eq!(
-        socket_addr.to_string(),
-        exptected_addr,
-        "Socket address does not match expected: {} != {}",
-        socket_addr,
-        exptected_addr
+#[test]
+// Test with a custom configuration
+pub fn test_get_socket_address_with_custom_config() {
+    test_socket_address!(config!(
+        r#"
+        host = "192.168.0.1"
+        port = 8080
+        sites_directory = "./sites"
+        "#
+    ));
+}
+
+#[test]
+// Test with a configuration that has a reserved port
+pub fn test_get_socket_address_with_invalid_config() {
+    test_socket_address!(
+        config!(
+            r#"
+            host = "0.0.0.0"
+            port = 80
+            sites_directory = "./sites"
+            "#
+        ),
+        reserved
     );
 }
