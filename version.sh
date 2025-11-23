f032313b8fbdae226d4fe6b246327919c95c8117
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-11-23 21:56:34 +0000

    Minor updates to version.sh

--- version.sh
+++ version.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-export VERSION=$(cargo metadata --format-version 1 --no-deps | jq -r '.packages[0].version')
+PATH="$HOME/.cargo/bin:$PATH"
+VERSION=$(cargo metadata --format-version 1 --no-deps | jq -r '.packages[0].version')
+
+# IF show is passed as an argument, print the version
+if [[ "$1" == "show" ]]; then
+	echo "$VERSION"
+fi
+
+export VERSION
